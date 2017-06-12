c1a8d776f2491ad4135fb20910c6f8fe9895fe78
eicyan <1194289@qq.com>
2017-06-12 22:09:58 +0800

    check user token

---
+++ app/config/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by eicyan on 2017/6/12.
+ */
+export default {
+    appInfo:{
+        name:'WildDream',
+    },
+    apiDomain:'https://api.cnblogs.com/'
+};
+
+export const storageKey = {
+    OFFLINE_POSTS: "OFFLINE_POSTS",
+    USER_TOKEN: "USER_TOKEN",
+    TAIL_CONTENT: "TAIL_CONTENT",
+    TAIL_ENABLED: "TAIL_ENABLED"
+};
--- app/view/startup.js
+++ app/view/startup.js
@@ -15,8 +15,11 @@ import {
 } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import TimerMixin from 'react-timer-mixin';
+import * as Animatable from 'react-native-animatable';
 import * as ConfigAction from '../action/config';
 import * as UserAction from '../action/user';
+import Config, { storageKey } from '../config';
 import ViewPage from '../component/view';
 
 class StartupPage extends Component {
@@ -26,9 +29,77 @@ class StartupPage extends Component {
 
     }
 
-    render() {
+    componentWillUnmount() {
+        this.timer && TimerMixin.clearTimeout(this.timer);
+    }
+
+    checkUserToken(){
+        console.log('checkUserToken');
+        this.props.configAction.getConfig({
+            key: storageKey.USER_TOKEN,
+            resolved: (data)=>{
+                console.log('resolved');
+                if(data && data.access_token && data.username && data.password){
+                    this.refreshUserToken(data);
+                }else{
+                    this.onCheckUserTokenRejected();
+                }
+            },
+            rejected: (data)=>{
+                console.log('rejected');
+                this.onCheckUserTokenRejected();
+            }
+        });
+    }
+
+    onCheckUserTokenRejected(){
+        console.log('onCheckUserTokenRejected');
+        NetInfo.fetch().then((netinfo=> {
+            if(netinfo.toUpperCase() != 'NONE'){
+                this.showLoginModal();
+            }
+        }));
+    }
+
+    refreshUserToken(tokenData){
+        console.log('refreshUserToken');
+        //const { userAction, router } = this.props;
+        //userAction.login({
+        //    username: tokenData.username,
+        //    password: tokenData.password,
+        //    resolved: (data)=>{
+        //        data.username = tokenData.username;
+        //        data.password = tokenData.password;
+        //        this.handleLoginResolved(data);
+        //    },
+        //    rejected: (data)=>{
+        //        this.showLoginModal();
+        //    }
+        //});
+    }
+
+    showLoginModal(){
+        //this.setState({
+        //    modalBackdropVisiable: true
+        //});
+        this.timer = TimerMixin.setTimeout(() => {
+            this.props.router.resetTo(ViewPage.login());
+        }, 500);
+    }
+
+    onPageContentShow(){
+        console.log('onPageContentShow');
+        this.timer = TimerMixin.setTimeout(() => {
+            this.checkUserToken();
+        }, 300);
+    }
+
+    renderContent(){
+        console.log('renderContent');
         return (
-            <View style={styles.container}>
+            <Animatable.View
+                onAnimationEnd = {()=>this.onPageContentShow() }
+                animation="fadeInDown">
                 <Text style={styles.welcome}>
                     Hello World!~~
                 </Text>
@@ -39,6 +110,14 @@ class StartupPage extends Component {
                     Double tap R on your keyboard to reload,{'\n'}
                     Shake or press menu button for dev menu
                 </Text>
+            </Animatable.View>
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                { this.renderContent() }
             </View>
         );
     }
